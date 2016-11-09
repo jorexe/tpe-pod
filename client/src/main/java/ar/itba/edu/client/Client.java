b17aba0efa6a265abad4b4722d3c84f7c73bfd2c
Jorge Gomez <gomezjorge.exequiel@gmail.com>
2016-11-09 20:00:05 -0300

    +Project init.

---
+++ client/src/main/java/ar/itba/edu/client/Client.java
@@ -0,0 +1,12 @@
+package ar.itba.edu.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private static Logger logger = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args) {
+        logger.info("5205552108 Client Starting ...");
+    }
+}
---
+++ server/src/main/java/ar/itba/edu/server/Server.java
@@ -0,0 +1,12 @@
+package ar.itba.edu.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("5205552108 Server Starting ...");
+    }
+}
