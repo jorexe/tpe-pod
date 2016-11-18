aecaf197979013381fcd444c897b8b3f01d43a0e
Jorge Gomez <gomezjorge.exequiel@gmail.com>
2016-11-17 21:16:53 -0300

    +Added province counter key predicate.

---
+++ api/src/main/java/mbaracus/query4/mr/ProvinceCounterKeyPredicate.java
@@ -0,0 +1,21 @@
+package mbaracus.query4.mr;
+
+import com.hazelcast.mapreduce.KeyPredicate;
+import mbaracus.model.CensoTuple;
+
+/**
+ * Created by jorexe on 17/11/16.
+ */
+public class ProvinceCounterKeyPredicate implements KeyPredicate<CensoTuple> {
+
+    private String prov;
+
+    public ProvinceCounterKeyPredicate(String prov) {
+        this.prov = prov;
+    }
+
+    @Override
+    public boolean evaluate(CensoTuple s) {
+        return prov.equals(s.getNombreprov());
+    }
+}
